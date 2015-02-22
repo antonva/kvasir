d4aebe00f001569f1284b2a17b9d207354f87274
Anton Vilhelm Ásgeirsson <anton@antonvilhelm.is>
2015-02-22 17:30:24 +0000

    Nothing works. /wrists

--- CREATE.sql
+++ CREATE.sql
@@ -1,24 +1,101 @@
 CREATE TABLE IF NOT EXISTS item (
-	product_id INTEGER NOT NULL, 
-	name VARCHAR(255) NOT NULL,
-	price INTEGER NOT NULL,
-	abv FLOAT REAL NOT NULL,
-	volume INTEGER NOT NULL,
-	vintage INTEGER,
-	importer VARCHAR(255) NOT NULL,
-	country VARCHAR(255) NOT NULL,
-	category VARCHAR(255) NOT NULL,
-	stock updated 
+	id            INTEGER NOT NULL, 
+	name          TEXT NOT NULL,
+	volume        INTEGER NOT NULL,
+	price         INTEGER NOT NULL,
+	abv           REAL NOT NULL,
+	vintage       INTEGER,
+	importer      TEXT NOT NULL,
+	country       TEXT NOT NULL,
+	category      TEXT NOT NULL,
+	stock_updated TIMESTAMP NOT NULL,
 
-	PRIMARY KEY (product_id)
+	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS distances (
-	system_1 VARCHAR(255) NOT NULL,
-	system_2 VARCHAR(255) NOT NULL,
-	distance REAL NOT NULL,
+CREATE TABLE IF NOT EXISTS inventory (
+	product_id INTEGER NOT NULL,
+	store      VARCHAR(255) NOT NULL,
+	stock      INTEGER NOT NULL,
 
-	PRIMARY KEY (system_1, system_2),
-	FOREIGN KEY (system_1) REFERENCES systems (name),
-	FOREIGN KEY (system_2) REFERENCES systems (name)
+	PRIMARY KEY (product_id),
+	FOREIGN KEY (product_id) REFERENCES item (id)
 );
+
+DROP FUNCTION IF EXISTS upsert_item(
+	u_id            int, 
+	u_name          text,
+	u_volume        int,
+	u_price         int,
+	u_abv           real,
+	u_vintage       int,
+	u_importer      text,
+	u_country       text,
+	u_category      text,
+	u_stock_updated timestamp
+);
+
+-- We upsert now.
+CREATE FUNCTION upsert_item(
+	u_id            int, 
+	u_name          text,
+	u_volume        int,
+	u_price         int,
+	u_abv           real,
+	u_vintage       int,
+	u_importer      text,
+	u_country       text,
+	u_category      text,
+	u_stock_updated timestamp) returns void as
+$$
+BEGIN
+	LOOP
+		-- first try to update the key
+		UPDATE item SET 
+		name          = u_name,
+		volume        = u_volume,
+		price         = u_price,
+		abv           = u_abv,
+		vintage       = u_vintage,
+		importer      = u_importer,
+		country       = u_country, 
+		category      = u_category,
+		stock_updated = u_stock_updated
+		WHERE id = u_id;
+		IF found THEN
+			RETURN;
+		END IF;
+		-- not there, so try to insert the key
+		-- if someone else inserts the same key concurrently,
+		-- we could get a unique-key failure
+		BEGIN
+			INSERT INTO db(
+				id, 
+				name, 
+				volume,
+				price, 
+				abv, 
+				vintage, 
+				importer,
+				country,
+				category,
+				stock_updated
+			) VALUES (
+				u_name,
+				u_volume,
+				u_price,
+				u_abv,
+				u_vintage,
+				u_importer,
+				u_country, 
+				u_category,
+				u_stock_updated
+			);
+			RETURN;
+		EXCEPTION WHEN unique_violation THEN
+		-- Do nothing, and loop to try the UPDATE again.
+		END;
+	END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
--- index.js
+++ index.js
@@ -2,8 +2,6 @@ var _    = require('lodash');
 var xray = require('x-ray');
 var pg   = require('pg');
 
-var conString = "postgres://kvasir@localhost/mjodr";
-
 var base_url = "http://www.vinbudin.is/Desktopdefault.aspx/tabid-54?productID="
 
 var url = process.argv[2];
@@ -28,18 +26,54 @@ xray(url)
 		}
 	}])
 	.run(function(err, array) {
-		var client = new pg.Client(conString)
+		var a      = array[0];
+		var s      = a.stock_updated.slice(1,-1);
+		var tmp    = s.split(" ") ;
+		tmp[0]     = tmp[0].split(".");
+		tmp[1]     = tmp[1].split(":");
+		var second, minute, hour, day, month, year;
+		second = "00";	
+		minute = tmp[1][1];
+		hour   = tmp[1][0];
+		day    = tmp[0][0];
+		month  = ('0'+ tmp[0][1]).slice(-2);
+		year   = tmp[0][2];
+		var date = year+"-"+month+"-"+day+" "+hour+":"+minute+":"+second;
+
+		var client = new pg.Client({
+			user: 'kvasir',
+			database: 'mjodr',
+			host: 'localhost'
+		});
+
 		client.connect(function(err) {
 			if(err) {
-				return console.error('could not connect to postgres', err);
+				return console.error(err);
 			}
-			client.query("", function(err, result) {
+			client.query({
+				text: "SELECT upsert_item($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);",
+				values: [ 
+						a.product_id,
+						a.product_name,
+						a.price,
+						a.abv,
+						a.volume,
+						a.vintage,
+						a.importer,
+						a.country,
+						a.category,
+						a.description,
+						a.stock_updated
+					]
+			}, function(err, result) {
 				if(err) {
-					return console.error('error running query', err);
+					console.log(JSON.stringify(err));
+					return console.error(err)
 				}
+				done;
 				//do stuff.
-				client.end();
 			});
 		});
+		client.end();
 
 	})
